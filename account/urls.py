f4f9d40afc392b63ee32c971dc9ddd2c7d7656ad
mrmohsensami <mrmohsensami@gmail.com>
2022-06-21 16:36:34 +0430

    Login form added

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 
+app_name = 'account'
 urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='home')
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
 
@@ -22,3 +23,23 @@ class RegisterView(View):
             return redirect('social:home')
         return render(request, self.template_name, {'form': form})
 
+
+class LoginView(View):
+    form_class = UserLoginForm
+    template_name = 'account/login.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged in successfully', 'success')
+                return redirect('social:home')
+            messages.error(request, 'username or password is wrong', 'warning')
+        return render(request, self.template_name, {'form': form})
