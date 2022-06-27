b6e67a18d3aabd0fa78fda7a5bc4978c8e7eb6f0
mrmohsensami <mrmohsensami@gmail.com>
2022-06-27 10:25:33 +0430

    CommentCreateForm in detail page and add method_decorator for method post beacuase only login users can post comment.

--- post/views.py
+++ post/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Post
 from django.contrib import messages
-from .forms import PostCreateUpdateForm
+from .forms import PostCreateUpdateForm, CommentCreateForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.text import slugify
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class PostListView(View):
@@ -26,10 +28,26 @@ class PostListView(View):
 
 
 class PostDetailView(View):
+    form_class = CommentCreateForm
+
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'], slug=kwargs['post_slug'])
+        return super().setup(self, request, *args, **kwargs)
+
     def get(self, request, post_id, post_slug):
-        post = get_object_or_404(Post, pk=post_id, slug=post_slug)
-        comments = post.pcomments.filter(is_reply=False)
-        return render(request, 'post/detail.html', {'post': post, 'comments': comments})
+        comments = self.post_instance.pcomments.filter(is_reply=False)
+        return render(request, 'post/detail.html', {'post': self.post_instance, 'comments': comments, 'form': self.form_class})
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = self.post_instance
+            new_comment.save()
+            messages.success(request, 'your comment submitted successfully', 'success')
+            return redirect('post:detail', self.post_instance.id, self.post_instance.slug)
 
 
 class PostDeleteView(View):
