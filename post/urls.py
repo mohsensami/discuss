d6e79c75f5fed36292e17c4f7858783aa3af3471
mrmohsensami <mrmohsensami@gmail.com>
2022-06-28 11:45:51 +0430

    Search => add search form and get query parameters  and show result in search.html

--- post/urls.py
+++ post/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('delete/<int:post_id>/', views.PostDeleteView.as_view(), name='delete'),
     path('update/<int:post_id>/', views.PostUpdateView.as_view(), name='update'),
     path('reply/<int:post_id>/<int:comment_id>/', views.PostAddReplyView.as_view(), name='add_reply'),
-    path('like/<int:post_id>/', views.PostLikeView.as_view(), name="post_like")
+    path('like/<int:post_id>/', views.PostLikeView.as_view(), name="post_like"),
+    path('search/', views.SearchView.as_view(), name="search"),
 ]
